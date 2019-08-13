67037fda8c5b113bac2789068bcb82c3b5ed5b3a
Fabian <fabrofabian@gmail.com>
2019-08-13 13:36:04 -0700

    Navbar is styled and works

--- composerinspire-frontend/src/components/Navbar.js
+++ composerinspire-frontend/src/components/Navbar.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import { Menu } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
+//  {/* <div>
+//             <ul id="nav-bar">
+//                 <li><a href="/">Home</a></li>
+//                 <li><a href="/compositions">My Compositions</a></li>
+                
+//             </ul>
+//         </div> */}
 
 const Navbar = () => {
+
     return(
-        <div>
-            <ul id="nav-bar">
-                <li><a href="/">Home</a></li>
-                <li><a href="/compositions">My Compositions</a></li>
-                {/* <li><a href="/signup">Sign Up</a></li>
-                <li><a href="/login">Log in</a></li> */}
-                
-            </ul>
-        </div>
+        <Menu>
+            <Menu.Item as={ Link } name='home' to='/'>Home</Menu.Item>
+            <Menu.Item as={ Link } to='/compositions' name='compositions'>Compositions</Menu.Item>
+        </Menu>
     )
 }
 
